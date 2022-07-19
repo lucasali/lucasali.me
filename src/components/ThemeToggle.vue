02dec571d5580731385a73e98c31bcca16db825a
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2022-07-19 02:56:55 -0300

    feat: theme switcher

---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { isDark, toggleDark } from '~/composables/dark'
+</script>
+
+<template>
+  <button @click="toggleDark()">
+    <div v-show="isDark" i-carbon-moon />
+    <div v-show="!isDark" i-carbon-sun />
+  </button>
+</template>
