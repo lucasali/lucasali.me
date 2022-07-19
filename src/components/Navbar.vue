0c3a669597f9906b93e8f2a1a4200aab6351389d
Lucas Sbardelotto Ali <lucassbardelottoali@gmail.com>
2022-07-19 02:57:30 -0300

    feat: new navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <a href="https://github.com/lucasali" target="_blank" title="GitHub" class="lt-md:hidden">
+            <div i-carbon-logo-github />
+          </a>
+        </li>
+        <li>
+          <div>
+            <router-link to="/projects">
+              Projects
+            </router-link>
+          </div>
+        </li>
+        <li>
+          <ThemeToggle />
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
